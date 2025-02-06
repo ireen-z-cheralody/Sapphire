5632ff4bd67807b4ae1bee9171f2ac1e5dd55c00
ireen-z-cheralody <ireen.z.cheralody@gmail.com>
2025-02-06 08:49:57 -0500

    Title Screen

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+function startGame() {
+    alert("The game has started! You are now in the enchanted forest...");
+    // Here you can redirect to the next scene or load the first puzzle page.
+}
